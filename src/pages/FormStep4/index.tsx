da960a511b1cff60c8d53f14f9618c9e0bc90df2
Bruno Leonardo Sancho Ventura <86660522+brunoventura123@users.noreply.github.com>
2021-11-03 09:56:47 -0300

    New commit

--- src/pages/FormStep4/index.tsx
+++ src/pages/FormStep4/index.tsx
@@ -30,8 +30,8 @@ export const FormStep4 = () => {
     return(
         <Theme>
             <C.Container>
-                <h2>😁Cadastro finalizado!🤗</h2>
-                <h1>Agora é só aguardar seu emprego.</h1>
+                <h2>😁 Cadastro finalizado! 🤗</h2>
+                <h1>Agora é só aguardar seu emprego!</h1>
                 <hr /><br />
                 <p>Nome Completo:</p> {state.name}
                 <hr />
